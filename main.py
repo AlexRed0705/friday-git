28b6f30473736d233d184dc322b69d7569122ee3
lexmarkin <markinaleksandr386@gmail.com>
2024-12-02 19:30:16 +0300

    Added my_superfunction()

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print('My first Git program!')
-print('Other string')
-print('Opa')
+def my_superfunction():
+    print('What an awesome print!')
+
+
+def main():
+    print('My first Git program!')
+    print('Other string')
+    print('Opa')
+    my_superfunction()
+
+if __name__ == '__main__':
+    main()
